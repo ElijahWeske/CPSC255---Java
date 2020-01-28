47c5e868c20f9e5e63ca27bd5987c04438be3640
Elijah Weske <54817873+ElijahWeske@users.noreply.github.com>
2020-01-28 00:54:17 -0500

    Update BasicArrays.java

--- CS Recitation/BasicArrays.java
+++ CS Recitation/BasicArrays.java
@@ -1,4 +1,24 @@
 /**
 * Array method for second recitation section.
-*
+* 
+* Per Dr. Lambert: the problem this week that I would like you to solve is ---
+* Method: countMatches(item, array, start, end). 
+* countMatches(4, {5, 6, 7}, 0, 3) would return 0. 
+* countMatches(7, {5, 6, 7}, 0, 3) would return 1. 
+* countMatches(7, {5, 6, 7}, 0, 2) would return 0. 
+* Start is inclusive. End is exclusive.
 */
+
+public class Solution{
+  public int countMatches(int item, int[] array, int start, int end){
+    int count = 0;
+    
+    // What is the Big O Notation of this...? How many times am I going through each item?
+    // Is there a faster way?
+    for (int i = start; i < end; i++){
+      if (array[i] == item) count++;
+    }
+    
+    return count;
+  }
+}
